0665ed012fef6b0ec4039251a2c96db224c4a0cc
Andrey Pankratov <i@doq.su>
2020-02-24 23:19:47 +0300

    BS-1: test route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routes/user');
 
 var app = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 80;
 const URL = "mongodb://localhost:27017";
 
 const db = mongoose.connection;
