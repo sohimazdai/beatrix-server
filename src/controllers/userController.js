c566df83fa2836b906273fb79b7227a629ad9679
Andrey Pankratov <sohimazdai@gmail.com>
2020-05-05 16:22:53 +0300

    bs5/firebase-bye

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -79,10 +79,6 @@ class UserController {
                 await newUser.save();
                 res.send(newUser);
             } else {
-                //start
-                user.set('authType', requestUser.authType);
-                await user.save();
-                //end
                 res.send(user);
             }
         } catch (e) {
