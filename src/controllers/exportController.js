aba1eb4c3008e7f8875dd1566594676fb55ee08c
Andrey Pankratov <sohimazdai@gmail.com>
2020-07-09 10:47:45 +0300

    feature/export add download xlsx

--- src/controllers/exportController.js
+++ src/controllers/exportController.js
@@ -1,17 +1,32 @@
-const fs = require("fs");
 const Excel = require('exceljs');
-
+const fs = require('fs');
 
 const UserModel = require('../models/userModel');
 const NoteModel = require('../models/noteModel');
+const { title } = require('process');
 
 class ExportController {
+  static async download(req, res) {
+    try {
+      const userId = req.body.userId;
+
+      res.status(200);
+      res.download(__dirname, `${userId}.xlsx`);
+      res.end();
+    } catch (error) {
+      console.log(__filename + " catch error: ", error && error.message);
+      res.status(500);
+      res.send(error);
+    }
+  };
+
   static async exportNotes(req, res) {
     try {
       const userId = req.body.userId;
       const titles = req.body.titles || {};
       const from = req.body.from;
       const to = req.body.to;
+      const stats = req.body.stats || {};
 
       const user = await UserModel.findOne({ id: userId });
       const userNotes = await NoteModel.find({ userId });
@@ -20,57 +35,75 @@ class ExportController {
         .filter(note => note.date > from)
         .filter(note => note.date < to);
 
-      if (user) {
-        const options = {
-          filename: `./temp/${userId}.xlsx`,
-          useStyles: true,
-          useSharedStrings: true
-        };
+      const fileName = __dirname + `/${userId}.xlsx`;
+      const options = {
+        filename: fileName,
+        useStyles: true,
+        useSharedStrings: true
+      };
 
-        const workbook = new Excel.stream.xlsx.WorkbookWriter(options);
-        const worksheet = workbook.addWorksheet('My Sheet');
-        worksheet.columns = [
-          { header: titles.date, key: 'date', width: 12 },
-          { header: titles.time, key: 'time', width: 12 },
-          { header: titles.glucose, key: 'glucose', width: 12 },
-          { header: titles.breadUnits, key: 'breadUnits', width: 12 },
-          { header: titles.insulin, key: 'insulin', width: 12 },
-          { header: titles.longInsulin, key: 'longInsulin', width: 12 },
-          { header: titles.comment, key: 'comment', width: 12 },
-        ];
+      const workbook = new Excel.stream.xlsx.WorkbookWriter(options);
+      const statisticsSheet = workbook.addWorksheet('Statistics');
+      statisticsSheet.columns = [
+        { header: titles.statisticsName, key: 'name', width: 40 },
+        { header: titles.statisticsNameValue, key: 'value', width: 10 },
+      ];
 
-        sortedUserNotes.map(note => {
-          const date =
-            (new Date(note.date).getDate() < 10 ? '0' + new Date(note.date).getDate() : new Date(note.date).getDate()) +
-            '.' +
-            (new Date(note.date).getMonth() < 9 ? '0' + (new Date(note.date).getMonth() + 1) : (new Date(note.date).getMonth() + 1)) +
-            '.' +
-            (new Date(note.date).getFullYear());
-          const time =
-            (new Date(note.date).getHours() < 10 ? '0' + (new Date(note.date).getHours()) : (new Date(note.date).getHours())) +
-            ':' +
-            (new Date(note.date).getMinutes() < 10 ? '0' + (new Date(note.date).getMinutes()) : (new Date(note.date).getMinutes()));
+      addGeneralStatistics(stats, titles, statisticsSheet, from, to);
+      addStatsRows(stats, titles, statisticsSheet);
+      statisticsSheet.commit();
 
-          worksheet.addRow({
-            date,
-            time,
-            glucose: note.glucose,
-            breadUnits: note.breadUnits,
-            insulin: note.insulin,
-            longInsulin: note.longInsulin,
-            comment: note.comment,
-          })
+      const worksheet = workbook.addWorksheet('Notes');
+
+      worksheet.columns = [
+        { header: titles.date, key: 'date', width: 12 },
+        { header: titles.time, key: 'time', width: 12 },
+        { header: titles.glucose, key: 'glucose', width: 12 },
+        { header: titles.breadUnits, key: 'breadUnits', width: 12 },
+        { header: titles.insulin, key: 'insulin', width: 12 },
+        { header: titles.longInsulin, key: 'longInsulin', width: 12 },
+        { header: titles.comment, key: 'comment', width: 12 },
+      ];
+
+      sortedUserNotes.map(note => {
+        const date =
+          (new Date(note.date).getDate() < 10 ? '0' + new Date(note.date).getDate() : new Date(note.date).getDate()) +
+          '.' +
+          (new Date(note.date).getMonth() < 9 ? '0' + (new Date(note.date).getMonth() + 1) : (new Date(note.date).getMonth() + 1)) +
+          '.' +
+          (new Date(note.date).getFullYear());
+        const time =
+          (new Date(note.date).getHours() < 10 ? '0' + (new Date(note.date).getHours()) : (new Date(note.date).getHours())) +
+          ':' +
+          (new Date(note.date).getMinutes() < 10 ? '0' + (new Date(note.date).getMinutes()) : (new Date(note.date).getMinutes()));
+
+        worksheet.addRow({
+          date,
+          time,
+          glucose: note.glucose,
+          breadUnits: note.breadUnits,
+          insulin: note.insulin,
+          longInsulin: note.longInsulin,
+          comment: note.comment,
         })
-        worksheet.commit();
+      })
+      worksheet.commit();
 
-        workbook.commit().then(function () {
-          console.log('xls file is written.');
-        });
-      }
+      workbook.commit().then(function () {
+        console.log('xls file is written.');
+      });
 
-      res.type('application/json');
-      res.status(200);
-      res.send('OK');
+      var stat = fs.statSync(fileName);
+
+      res.writeHead(200, {
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'Content-Length': stat.size
+      });
+
+      var readStream = fs.createReadStream(fileName);
+      // We replaced all the event handlers with a simple call to readStream.pipe()
+      readStream.pipe(res);
+      res.end();
     } catch (error) {
       console.log(__filename + " catch error: ", error && error.message);
       res.status(500);
@@ -79,23 +112,41 @@ class ExportController {
   }
 }
 
-module.exports = ExportController;
+function addGeneralStatistics(stats, titles, statisticsSheet, from, to) {
+  statisticsSheet.addRow({
+    name: titles.dateFrom,
+    value: new Date(from),
+  });
+  statisticsSheet.addRow({
+    name: titles.dateTo,
+    value: new Date(to),
+  });
+  statisticsSheet.addRow({
+    name: titles.totalNotes,
+    value: stats.totalNotes,
+  });
+};
 
-// const allUsers = require('./sample.json');
-// const fs = require('fs');
-// const app = require('express')();
-// const json2xls = require('json2xls');
-// const filename = 'sample.xlsx';
-// app.listen(5050, () => {
-//   console.log("app is running on port 5050");
-//   convert();
-// })
-// const convert = function () {
-//   const xls = json2xls(allUsers);
-//   fs.writeFileSync(filename, xls, 'binary', (err) => {
-//     if (err) {
-//       console.log("writeFileSync :", err);
-//     }
-//     console.log(filename + " file is saved!");
-//   });
-// }
+function addStatsRows(stats, titles, statisticsSheet) {
+  statisticsSheet.addRow({
+    name: titles.averageGlucose,
+    value: stats.averageGlucose,
+  });
+
+  statisticsSheet.addRow({
+    name: titles.dailyAverageBreadUnits,
+    value: stats.dailyAverageBreadUnits,
+  });
+
+  statisticsSheet.addRow({
+    name: titles.dailyAverageInsulin,
+    value: stats.dailyAverageInsulin,
+  });
+
+  statisticsSheet.addRow({
+    name: titles.dailyAverageLongInsulin,
+    value: stats.dailyAverageLongInsulin,
+  });
+}
+
+module.exports = ExportController;
--- src/routes/exportRoutes.js
+++ src/routes/exportRoutes.js
@@ -5,5 +5,6 @@ const ExportController = require('../controllers/exportController');
 const router = Router();
 
 router.post('/export', ExportController.exportNotes);
+router.get('/export/download', ExportController.download);
 
 module.exports = router;
